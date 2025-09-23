c7839c1e0c4daa4b6f6d4cb7fcb4d5ba84f0bef7
Hafizfauzi02 <hafizfauzi60@gmail.com>
2025-09-23 22:39:34 +0800

    map commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'shared/theme/theme.dart';
 import 'features/tutor/main_tutor.dart';
 import 'features/game/quiz1.dart';
-
+import 'features/tutor/main_tutor.dart';
+import 'map.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,8 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
 
-      home: const quiz1(),
-
+      home: const MapChickgu(),
     );
   }
 }
