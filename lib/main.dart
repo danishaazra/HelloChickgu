4e2d156b75961c1bcf958275d1a82108e93327aa
Saidahtul Shaharudin <Saidahtulshaharudin@gmail.com>
2025-09-24 00:09:37 +0800

    sai

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:hellochickgu/features/game/level_page.dart';
+// import 'package:hellochickgu/features/game/quiz2.dart';
 import 'shared/theme/theme.dart';
 import 'features/game/quiz1.dart';
+import 'features/game/training_page.dart';
+import 'features/game/quiz4.dart';
 
 void main() {
   GoogleFonts.config.allowRuntimeFetching = false;
@@ -16,7 +21,10 @@ class MyApp extends StatelessWidget {
       title: 'Hello Chickgu!',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const quiz1(),
+      home: const quiz4(),
+      routes: {
+        '/training': (context) => const TrainingPage(),
+      },
     );
   }
 }
